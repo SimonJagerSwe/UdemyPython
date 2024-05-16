a70e23a826c7f1d194bf43562b849925b659387c
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-16 17:26:44 +0200

    Commited new stuff

--- polymorphism.py
+++ polymorphism.py
@@ -1,2 +1,28 @@
 ########## POLYMORPHISM ##########
 
+class User:
+    def sign_in(self):
+        print('Logged in')
+
+class Wizard(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+        
+        
+    def attack(self):
+        print(f'Attacking with power of {self.power}')
+
+class Archer(User):
+    def __init__(self, name, num_arrows):
+        self.name = name
+        self.num_arrows = num_arrows
+
+    def attack(self):
+        print(f'Attacking with arrows: arrows left: {self.num_arrows}')
+
+wizard1 = Wizard('Merlin', 60)
+archer1 = Archer('Robin', 30)
+
+def player_attack(char):
+    char.attack()
