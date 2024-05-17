946f0f80bda17eb4005fc199bfa9c722444917c5
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-17 16:50:21 +0200

    Commiting __str__()

--- dunder_methods.py
+++ dunder_methods.py
@@ -1,2 +1,13 @@
 ########## DUNDER METHODS ##########
 
+class Toy():
+    def __init__(self, colour, age):
+        self.colour = colour
+        self.age = age
+
+    def __str__(self):
+        return f'{self.colour}'
+
+action_figure = Toy('Red', 0)
+print(action_figure.__str__())
+print(str(action_figure))
