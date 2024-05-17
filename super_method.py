11e2a85fef3cca3fa0f5408b5c25bf6d563dfd9e
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-17 16:44:57 +0200

    latest update

--- super_method.py
+++ super_method.py
@@ -8,12 +8,14 @@ class User(object):
         print('Logged in!')
 
 class Wizard(User):
-    def __init__(self, name, power):
+    def __init__(self, name, power, email):
+        # User.__init__(self, email)        # OPTION 1
+        super().__init__(email)
         self.name = name
         self.power = power
 
     def attack(self):
         print(f'Attacking with power of {self.power}')
 
-wizard1 = Wizard('Merlin', 60)
-print(wizard1.email)
+wizard1 = Wizard('Merlin', 60, 'merlin@gmail.com')
+print(wizard1.email)      # WILL THROW ERROR WITHOUT User.__init__(self, email) or super().__init__(self, email)
