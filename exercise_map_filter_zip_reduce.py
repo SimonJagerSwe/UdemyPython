993e3f5e3fab2c297862b998ea2745bdf0b85966
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-18 15:12:44 +0200

    Added Udemy solutions

--- exercise_map_filter_zip_reduce.py
+++ exercise_map_filter_zip_reduce.py
@@ -2,6 +2,7 @@
 from functools import reduce
 
 #1 Capitalize all of the pet names and print the list
+# My solution
 my_pets = ['sisi', 'bibi', 'titi', 'carla']
 
 def capitalise(name):
@@ -13,8 +14,15 @@ def capitalise(name):
 print(capitalise(my_pets)) 
 
 
+# Udemy solution
+def capitalize(string):
+    return string.upper()
+
+print(list(map(capitalize, my_pets)))
+
 
 #2 Zip the 2 lists into a list of tuples, but sort the numbers from lowest to highest.
+# My solution
 my_strings = ['a', 'b', 'c', 'd', 'e']
 my_numbers = [5,4,3,2,1]
 my_num_reverse = sorted(my_numbers)
@@ -23,16 +31,22 @@ new_tuple = tuple(zip(my_strings, my_num_reverse))
 print(new_tuple)
 
 
+# Udemy solution
+print(list(zip(my_strings, sorted(my_numbers))))
+
+
 #3 Filter the scores that pass over 50%
+# Both solutions same
 scores = [73, 20, 65, 19, 76, 100, 88]
 
 def check_pass(grade):
-    return grade >= 50
+    return grade > 50
 
 print(list(filter(check_pass, scores)))
 
 
 #4 Combine all of the numbers that are in a list on this file using reduce (my_numbers and scores). What is the total?
+# My solution
 all_num = (my_numbers + scores)
 
 def accumulator(acc, item):
@@ -41,3 +55,10 @@ def accumulator(acc, item):
 
 total = reduce(accumulator, all_num, 0)
 print(total)
+
+
+# Udemy solution
+def accumulator(acc, item):
+    return acc + item
+
+print(reduce(accumulator, (my_numbers + scores)))
