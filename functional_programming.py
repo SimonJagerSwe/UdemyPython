6a62e9e17ccc519518db5d5fd21f2da0e63e8b01
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-18 14:39:02 +0200

    Done

--- functional_programming.py
+++ functional_programming.py
@@ -25,17 +25,51 @@ def multiply_by2(list):
 print(multiply_by2([1, 2, 3]))
 '''
 
+
 ##### MAP #####
+'''
 def multiply_by2(list):
     new_list = []
     for item in list:
         new_list.append(item*2)
     return new_list
+'''
 
-print(map(multiply_by2, [1, 2, 3]))
+'''
+def multiply_by2(item):
+    return item*2
+
+print(list(map(multiply_by2, [1, 2, 3])))
+'''
+
 
 ##### FILTER #####
+'''
+my_list = [1, 2, 3]
+def check_odd(item):
+    return item % 2 != 0
+
+print(list(filter(check_odd, my_list)))
+'''
+
 
 ##### ZIP #####
+'''
+my_list = [1, 2, 3]
+your_list = [10, 20, 30]
+
+print(list(zip(my_list, your_list)))
+'''
+
 
 ##### REDUCE #####
+from functools import reduce
+
+my_list = [1, 2, 3]
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
+print(reduce(accumulator, my_list, 0))
