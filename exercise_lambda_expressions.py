a2466daaef847d4ccbd9622411b3bc56bc129535
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-18 15:34:57 +0200

    Added Udemy solution to first challenge

--- exercise_lambda_expressions.py
+++ exercise_lambda_expressions.py
@@ -1,7 +1,13 @@
 ########## EXERCISE: LAMBDA EXPRESSIONS ##########
 # 1: Square
 my_list = [5, 4, 3]
+
+# My solution
 print(list(map(lambda num : num ** 2, my_list)))
+
+
+# Udemy solution
+print(list(map(lambda item : item * item, my_list)))
 
 # 2: List sorting
 a = [(0, 2), (3, 4), (9.9), (10, -1)]
