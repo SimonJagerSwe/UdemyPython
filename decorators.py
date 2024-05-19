c89a70d48b0d08cc9656cf57191793ac73ce834e
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-19 15:03:58 +0200

    Added more complex decorators

--- decorators.py
+++ decorators.py
@@ -11,6 +11,7 @@ print(greet)
 '''
 # Higher order functions
 # Functions that accept other functions as parameters
+'''
 def greet(func):
     func()
 
@@ -18,3 +19,43 @@ def greet2():
     def func():
         return 5
     return func
+    '''
+
+
+# Actual decorators (at last)
+'''
+def my_decorator(func):
+    def wrap_func():
+        print('*************')
+        func()
+        print('*************')
+    return wrap_func
+
+@my_decorator
+def hello():
+    print('Helloooooooo!')
+
+@my_decorator
+def bye():
+    print('See ya later!')
+
+hello()
+bye()
+
+a = my_decorator(hello)
+a()
+'''
+
+
+# More complex decorator
+def my_decorator(func):
+    def wrap_func(*args, **kwargs):
+        func(*args, **kwargs)
+    return wrap_func
+
+@my_decorator
+def hello(greeting, emoji = ':('):
+    print(greeting, emoji)
+
+hello('Hiiiiiii!')
+hello('Hiiiiiii!', ':)')
