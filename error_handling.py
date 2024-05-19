ace4a69651eadbe3faf4656a7901d03c9d5a8f5f
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-19 15:38:28 +0200

    Error handling added

--- error_handling.py
+++ error_handling.py
@@ -1,3 +1,30 @@
 ########## ERROR HANDLING ##########
 
 # print(1 + 'Hello') This exception will cause program to crash
+
+# Example 1
+'''
+while True:
+    try:
+        age = int(input('What\'s your age? '))
+        print(age)
+
+    except:
+        print('Invalid age format')
+
+    else:
+        print('Thank you!')
+        break
+'''
+
+
+# Example 2
+def sum(num1, num2):
+    try:
+        return num1 + num2
+    except:
+        print('Something went wrong')
+
+# print(sum('1', '2'))      WILL CONCATENATE 1 and 2 to 12
+# print(sum('1', 2))        WILL THROW ERROR, CAN'T CONCATENATE INT & STR
+print(sum('1', 2))
