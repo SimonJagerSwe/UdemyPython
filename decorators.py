8ec8718e2e3cd236d3c3ea9131c520460a9f7f2a
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-19 14:37:52 +0200

    Higher order functions added

--- decorators.py
+++ decorators.py
@@ -1,12 +1,20 @@
 ########## DECORATORS ##########
-
+'''
 def hello():
     print('Helloooooooooo!')
 
 greet = hello
-
 print(greet())
 
 del hello
-
 print(greet)
+'''
+# Higher order functions
+# Functions that accept other functions as parameters
+def greet(func):
+    func()
+
+def greet2():
+    def func():
+        return 5
+    return func
