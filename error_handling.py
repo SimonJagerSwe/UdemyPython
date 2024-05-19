c9648218ade1d035fbe2766f479992b66ad03068
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-19 15:41:38 +0200

    Error wrapping

--- error_handling.py
+++ error_handling.py
@@ -22,9 +22,10 @@ while True:
 def sum(num1, num2):
     try:
         return num1 + num2
-    except:
-        print('Something went wrong')
+    except TypeError as err:
+        print(f'Please enter numbers {err}')
 
 # print(sum('1', '2'))      WILL CONCATENATE 1 and 2 to 12
 # print(sum('1', 2))        WILL THROW ERROR, CAN'T CONCATENATE INT & STR
 print(sum('1', 2))
+print(sum(1, 2))
