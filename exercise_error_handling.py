61cafd1b43080f0cf44e1ec316f3e07d66bc7d6a
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-19 15:54:45 +0200

    Done

--- exercise_error_handling.py
+++ exercise_error_handling.py
@@ -1,14 +1,18 @@
 ########## EXERCISE: ERROR HANDLING ##########
 while True:
     try:
-        age = int(input('What is your age?'))
+        age = int(input('What is your age? '))
         10/age
     except ValueError:
         print('Please enter a number')
+        continue
     except ZeroDivisionError:
         print('Please enter an age higher than 0')
+        break
     else:
         print('Thank you!')
         break
     finally:
         print('Ok, I am finally done!')
+
+    print('Can you hear me?')
