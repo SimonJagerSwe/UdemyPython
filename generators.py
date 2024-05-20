59a4fa7da691677edf173f2f8f76531eb93f5707
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-20 15:36:12 +0200

    Created a range generator lolwtfroflcopter

--- generators.py
+++ generators.py
@@ -15,16 +15,53 @@ print(my_list)
 '''
 
 # Make your own generator
+'''
 def generator_function(num):
     for i in range(num):
         yield i * 2
 
-'''
+
 for item in generator_function(1000):
     print(item)
-'''
 
 g = generator_function(100)
 next(g)
 next(g)
 print(next(g))
+'''
+
+
+# Under the hood
+def special_for(iterable):
+    iterator = iter(iterable)
+    while True:
+        try:
+            print(iterator)
+            print(next(iterator))
+        except StopIteration:
+            break
+
+special_for([1, 2, 3])
+
+
+# Creating our own range()
+class MyGen():
+    current = 0
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        MyGen.current = self.first
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if MyGen.current < self.last:
+            num = MyGen.current
+            MyGen.current += 1
+            return num
+        raise StopIteration
+
+gen = MyGen(0, 100)
+for i in gen:
+    print(i)
