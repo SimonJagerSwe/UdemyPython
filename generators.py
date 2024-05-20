04828fea8599d16d932fd86ed07f73b058eab229
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-20 15:18:14 +0200

    yield and next explored

--- generators.py
+++ generators.py
@@ -1,2 +1,30 @@
 ########## GENERATORS ##########
+# Generators are iterable
 
+# range() is atype of generator
+'''
+def make_list(num):
+    result = []
+    for i in range(num):
+        result.append(i * 2)
+    return result
+
+my_list = make_list(100)
+print(my_list)
+
+'''
+
+# Make your own generator
+def generator_function(num):
+    for i in range(num):
+        yield i * 2
+
+'''
+for item in generator_function(1000):
+    print(item)
+'''
+
+g = generator_function(100)
+next(g)
+next(g)
+print(next(g))
