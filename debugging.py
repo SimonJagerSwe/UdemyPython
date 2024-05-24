134eb69bb3df303422992914af9948a449f1e78f
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 13:51:59 +0200

    explored pdb

--- debugging.py
+++ debugging.py
@@ -1,2 +1,17 @@
 ########## JOKES ##########
 
+# Linting
+# num + 4       VSC will tell us that num is not defined, this is linting
+
+# Always use an IDE or editor, they will have built in linting tools, autoformatting
+
+# Always read your errors
+# 4 + 'random string           Will throw a syntax error
+
+##### PDB/PYTHON DEBUGGER #####
+import pdb
+def add(num1, num2):
+    pdb.set_trace()
+    return num1, num2
+
+add(4, 'asdasd')
