f3e122a1499897eaa4d92e52b8721899cb19ed66
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 14:19:23 +0200

    used write to create new file

--- read_write_append.py
+++ read_write_append.py
@@ -4,7 +4,19 @@ with open('test.txt') as my_file:
 
 
 ##### WRITING TO A FILE #####
-
+# THIS OVERWRITES EXISTING TEXT IN FILE
+'''
 with open('test.txt', mode = 'r+') as my_file:
     text = my_file.write('Haha! Sneaking in and adding new text!')
+    print(text)
+'''
+
+with open('test.txt', mode = 'a') as my_file:
+    text = my_file.write('Haha! Sneaking in and adding new text!')
+    print(text)
+
+
+# Creating and writing in a new file
+with open('sad.txt', mode = 'w') as my_file2:
+    text = my_file2.write(':(')
     print(text)
