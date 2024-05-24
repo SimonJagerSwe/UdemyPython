d697cd8a8736d9aab4a4c8714aa0b51c79e08728
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 16:06:00 +0200

    Huh?

--- exercise_password_validation.py
+++ exercise_password_validation.py
@@ -1,1 +1,12 @@
 ########## EXERCISE: PASSWORD VALIDATION ##########
+import re
+
+pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+string = 'Simon'
+pattern2 = re.compile(r"[A-Za-z0-9$%#@]{8,}\d")
+password = 'hdjk'
+
+
+a = pattern.search(string)
+check = pattern2.fullmatch(password)
+print(check)
