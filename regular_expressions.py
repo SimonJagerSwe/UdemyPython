220e55daa0a4b707c8f1e0a55bbc859eaff27e92
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 15:17:27 +0200

    Single search works

--- regular_expressions.py
+++ regular_expressions.py
@@ -1,1 +1,17 @@
 ########## REGULAR EXPRESSIONS ##########
+import re
+
+string = 'Search inside of this text please'
+# This will work
+'''
+print('Search' in string)
+'''
+
+# print(re.search('this', string))
+
+# Single search
+a = re.search('this', string)
+print(a.span())
+print(a.start())
+print(a.end())
+print(a.group())
