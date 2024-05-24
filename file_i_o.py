b497c37c6d9d604fa181f66a0df049324b42a33f
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 14:02:03 +0200

    reading all text from txt-file

--- file_i_o.py
+++ file_i_o.py
@@ -1,2 +1,9 @@
 ########## FILE I/O ##########
 my_file = open('test.txt')
+
+print(my_file)
+print(my_file.read())
+print(my_file.read())
+my_file.seek(0)
+print(my_file.read())
+print(my_file.read())
