988c6d00b0aacae4cf9ccf770875c2663bb6e391
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 19:12:13 +0200

    Tests running ok, eh?

--- exercise_test.py
+++ exercise_test.py
@@ -1,18 +1,26 @@
 ########## EXERCISE: TEST ##########
 import random
 
-answer = random.randint(1, 10)
-while True:
-    try:
-        guess = int(input('Guess a number from 1 to 10: '))
-        if 0 < guess < 11:
-            if guess == answer:
-                print('Bing bong! You\'re a genius!')
-                break
+
+def run_guess(guess, answer):
+    if 0 < guess < 11:
+        if guess == answer:
+            print('Bing bong! You\'re a genius!')
+            return True
 
         else:
             print('Hey, nurmnurts, I said 1 to 10!')
+            return False
 
-    except ValueError:
-        print('Please enter a number, it\'s really not that hard!')
-        continue
+if __name__ == '__main__':
+    answer = random.randint(1, 10)
+
+    while True:
+        try:
+            guess = int(input('Guess a number from 1 to 10: '))
+            if (run_guess(guess, answer)):
+                break
+
+        except ValueError:
+            print('Please enter a number, it\'s really not that hard!')
+            continue
--- exercise_test_testfile.py
+++ exercise_test_testfile.py
@@ -1,2 +1,25 @@
 ########## EXERCISE: TEST ##########
 import unittest
+import exercise_test
+
+class TestGame(unittest.TestCase):
+    def test_input(self):
+        answer = 5
+        guess = 5
+        result = exercise_test.run_guess(answer, guess)
+        self.assertTrue(result)
+
+    def test_input_wrong_guess(self):
+        result = exercise_test.run_guess(5, 0)
+        self.assertFalse(result)
+
+    def test_input_wrong_number(self):
+        result = exercise_test.run_guess(5, 11)
+        self.assertFalse(result)
+
+    def test_input_string(self):
+        result = exercise_test.run_guess(5, '11')
+        self.assertFalse(result)
+
+if __name__ == '__main__':
+    unittest.main()
