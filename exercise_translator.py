a046679114150efbc38e6cff1085935b915dec11
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 14:55:07 +0200

    translated text into Spanish

--- exercise_translator.py
+++ exercise_translator.py
@@ -1,1 +1,11 @@
 ########## EXERCISE: TRANSLATOR ##########
+from translate import Translator
+
+translator = Translator(to_lang = 'es')
+try:
+    with open('test.txt', mode = 'r') as my_file:
+        text = my_file.read()
+        translation = translator.translate(text)
+        print(translation)
+except FileNotFoundError as e:
+    print('Check your filepath, you silly puppy!')
