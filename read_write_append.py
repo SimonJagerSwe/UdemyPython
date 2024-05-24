83a57caa2c6924c283a9a8b9f711b714826593ae
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-24 14:15:32 +0200

    writing to file from Python file

--- read_write_append.py
+++ read_write_append.py
@@ -1,2 +1,10 @@
 ########## FILE I/O ##########
+with open('test.txt') as my_file:
+    print(my_file.readlines())
 
+
+##### WRITING TO A FILE #####
+
+with open('test.txt', mode = 'r+') as my_file:
+    text = my_file.write('Haha! Sneaking in and adding new text!')
+    print(text)
