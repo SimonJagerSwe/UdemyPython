288b15f5b88b160f590d3616f3467c33ca51ea86
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-29 14:42:19 +0200

    Pikachu is all blurry

--- images.py
+++ images.py
@@ -1,7 +1,15 @@
 ########## IMAGES ##########
-from PIL import Image
+from PIL import Image, ImageFilter
 
+# Some methods
 img = Image.open('./images/pikachu.jpg')
 print(img)
 print(img.format)
 print(img.size)
+print(img.mode)
+print(dir(img))
+
+
+# Using filter
+filtered_image = img.filter(ImageFilter.BLUR)
+filtered_image.save('blur.png', 'png')
