198b027d04c402b3a3dbd68c39950b60c4f15d0b
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-29 15:02:49 +0200

    Challenge setup

--- image_converter.py
+++ image_converter.py
@@ -1,1 +1,18 @@
 ########## IMAGE CONVERTER ##########
+import sys
+import os
+from PIL import Image
+
+# Grabbing two arguments
+
+
+# Check if \new exists, if not, create it 
+
+
+# Loop through Images
+
+
+# Convert to png
+
+
+# Save to new folder
