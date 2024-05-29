b53fe82c580b0c373de98ca3d03b0bff9b0ea863
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-29 17:24:01 +0200

    Unable to complete due to 2FA

--- sending_emails.py
+++ sending_emails.py
@@ -1,1 +1,17 @@
 ########## SENDING EMAILS WITH PYTHON ##########
+import smtplib
+from email.message import EmailMessage
+
+email = EmailMessage()
+email['from'] = 'Simon Jäger'
+email['to'] = 'simonjager@hotmail.com'
+email['subject'] = 'You\'ve won a bajillion dollarz'
+
+email.set_content('I am a Python n00b...')
+
+with smtplib.SMTP(host = 'smtp.gmail.com', port = 587) as smtp:
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login('simonjager53@gmail.com', 'Talisker10')
+    smtp.send_message(email)
+    print('It worked!')
