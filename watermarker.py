6d716c49a66d3ed362329b75c7de3272102a9209
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-29 17:01:37 +0200

    Not working due to old PyPDF2 syntax not being usable

--- watermarker.py
+++ watermarker.py
@@ -1,4 +1,5 @@
 ########## WATERMARKER ##########
+##### BORKED DUE TO OUTADET PyPDF2 #####
 import PyPDF2
 
 base_file = PyPDF2.PdfReader(open('super.pdf', 'rb'))
@@ -6,3 +7,10 @@ watermark = PyPDF2.PdfReader(open('wtr.pdf', 'rb'))
 output = PyPDF2.PdfWriter()
 print(base_file)
 print(watermark)
+
+for i in range(len(base_file.pages())):
+    page = base_file.getPage(i)
+    page.mergePage(watermark.getPage(0))
+
+with open('watermarked_output.pdf', 'wb') as file:
+    output.write(file)
