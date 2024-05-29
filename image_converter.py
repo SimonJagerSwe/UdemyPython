e01de81e8fc57659f58517d0c504515d18d33517
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-29 15:34:53 +0200

    It works

--- image_converter.py
+++ image_converter.py
@@ -4,15 +4,20 @@ import os
 from PIL import Image
 
 # Grabbing two arguments
-
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
 
 # Check if \new exists, if not, create it 
-
+# print(os.path.exists(output_folder))
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
 # Loop through Images
-
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    clean_name = os.path.splitext(filename)[0]
+    # print(clean_name)
 
 # Convert to png
-
-
-# Save to new folder
+    img.save(f'{output_folder}{clean_name}.png', 'png')
+    print('All done!')
