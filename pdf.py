bb77cfae1d518eb7db8338690da30a2e34401245
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-29 16:43:57 +0200

    Merged PDFs

--- pdf.py
+++ pdf.py
@@ -1,5 +1,6 @@
 ########## PDFs ##########
 import PyPDF2
+import sys
 
 '''
 with open('dummy.pdf', 'rb') as file:
@@ -19,3 +20,13 @@ with open('dummy.pdf', 'rb') as file:
     
 
 # Merging exercise
+input = sys.argv[1:]
+
+def pdf_combiner(pdf_list):
+    merger = PyPDF2.PdfMerger()
+    for pdf in pdf_list:
+        print(pdf)
+        merger.append(pdf)
+    merger.write('super.pdf')
+
+pdf_combiner(input)
