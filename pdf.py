1f85def1c23d1e0c297d1e5400f392ec8d31e4c8
SimonJagerSwe <Simonjager@hotmail.com>
2024-05-29 16:38:37 +0200

    Stuff done

--- pdf.py
+++ pdf.py
@@ -1,7 +1,21 @@
 ########## PDFs ##########
 import PyPDF2
 
+'''
 with open('dummy.pdf', 'rb') as file:
     # print(file)
     reader = PyPDF2.PdfReader(file)
     print(len(reader.pages))
+    print(reader.pages[0])
+    page = reader.pages[0]
+    print(page)
+    page.rotate(90)
+
+    writer = PyPDF2.PdfWriter()
+    writer.add_page(page)
+    with open('tilt.pdf', 'wb') as new_file:
+        writer.write(new_file)
+'''
+    
+
+# Merging exercise
