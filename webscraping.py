7b1963786161877b4339b385b3d6406a92036ebb
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-23 19:19:02 +0200

    holy shit this works

--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,7 @@
 ########## WEB SCRAPING ##########
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 # print(res)
@@ -25,12 +26,21 @@ soup = BeautifulSoup(res.text, 'html.parser')
 
 links = soup.select('.titleline')
 votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
-def create_custom_hn(links, votes):
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key = lambda k : k['votes'])
+
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         title = links[idx].getText()
-        hn.append(title)
-    return hn
+        href = links[idx].get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title' : title, 'link' : href, 'votes' : points})        
+    return sort_stories_by_votes(hn)
 
-print(create_custom_hn(links, votes))
+pprint.pprint(create_custom_hn(links, subtext))
