e718acb5c3f4d8e97da86790fc002aa80a3ee8ad
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-24 14:30:51 +0200

    meh

--- web server/server.py
+++ web server/server.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 # print(__name__)
 
 @app.route('/')
 def hello_world():
    return 'Hello, Simon!'
+
+@app.route('/blog')
+def blog():
+   return 'These are my thoughts on blogs'
+
+@app.route('/blog/2020/dogs')
+def blog2():
+   return 'My dog is actually a cat'
+
+@app.route('/about.html')
+def about():
+   return render_template('about.html')
