240fd576942a415ca754f32655f728ee18263c77
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-24 14:10:31 +0200

    it works

--- web server/server.py
+++ web server/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-print(__name__)
+# print(__name__)
 
-# @app.route('/')
-# def hello_world():
-#    return 'Hello, World!'
+@app.route('/')
+def hello_world():
+   return 'Hello, Simon!'
