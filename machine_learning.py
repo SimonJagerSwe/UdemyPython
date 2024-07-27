48f849ba55c0674a8501745cd20739bf420701c0
SimonJagerSwe <Simonjager@hotmail.com>
2024-07-27 15:19:44 +0200

    steps of ML outlined

--- machine_learning.py
+++ machine_learning.py
@@ -1,1 +1,9 @@
 ########## MACHINE LEARNING ##########
+
+# STEPS:
+# 1: Import data
+# 2: Clean the data
+# 3: Split the data; training sets and test sets
+# 4: Create a model
+# 5: Check the output
+# 6: Improve the results
